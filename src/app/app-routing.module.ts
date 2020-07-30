105ce49443ce67d62af2ed61abc09420140e1949
JuanHenao24 <juan.1701811735@ucaldas.edu.co>
2020-07-29 19:11:01 -0500

    se añade las rutas de seguridad y las de app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path:'home',
+    component: DefaultComponent
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo: '/home'
+  },
+  /** this must be the last one (la ultima ruta) */
+  //si no existe la ruta que coloquemos, nos manda al /home 
+  {
+    path:'**',
+    redirectTo:'/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
