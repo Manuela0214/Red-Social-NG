419ee8c07aa754ff883ace55d1c6ed38c203702a
Juan Diego <juan.1701624716@ucaldas.edu.co>
2020-07-31 15:58:50 -0500

    Video 28, validación de datos en el registro

---
+++ src/app/config/forms-config.ts
@@ -0,0 +1,7 @@
+export namespace FormsConfig{
+    export const NAMES_MIN_LENGTH = 3;
+    export const PLACE_MIN_LENGTH = 3;
+    export const CELPHONE_MIN_LENGTH = 14;
+    export const INTERESES_MIN_LENGTH = 4;
+    export const GENERO_MAX_LENGTH = 1;
+}
