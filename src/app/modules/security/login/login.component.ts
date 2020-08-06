f91b893c356a115a781f0599b08455bbf319178a
Manuela Arcila <manu-142000@hotmail.com>
2020-08-06 07:58:09 -0500

    modificacion de la imagen del sidenav

--- src/app/modules/security/login/login.component.ts
+++ src/app/modules/security/login/login.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsConfig } from 'src/app/config/forms-config';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegistroModel } from 'src/app/models/registro.model';
+import { SecurityService } from '../../../services/security.service';
+import MD5 from '../../../../../node_modules/crypto-js/md5';
+//import MD5 from 'crypto-js';
+
+declare const showMessage: any;
+declare const initSidenav: any;
 
 @Component({
   selector: 'app-login',
@@ -6,10 +16,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  fgValidator: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private service: SecurityService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.FormBuilding();
+  }
+
+  FormBuilding() {
+    this.fgValidator = this.fb.group({
+      nombre_usuario: ['', [Validators.required, Validators.email]],
+      contrasena: ['', [Validators.required]]
+    });
+  }
+
+  /**
+   * Method to validate credentials of a user
+   */
+  LoginUsuarioFn() {
+    if (this.fgValidator.invalid) {
+      showMessage("Invalid form.");
+    } else {
+      //showMessage("Registering...");
+      let model = this.getLoginData();
+      console.log(model);
+      this.service.UsuarioLogin(model).subscribe(
+        data => {
+          //this.service.saveSessionData(data);
+          showMessage("Welcome to your account.");
+          this.router.navigate(['/home']);
+        },
+        error => {
+          showMessage("Invalid data.");
+        }
+      );
+    }
+  }
+
+  /**
+   * Get user data in a model
+   */
+  getLoginData(): RegistroModel {
+    let model = new RegistroModel();
+    model.nombre_usuario = this.fgv.nombre_usuario.value;
+    model.contrasena = MD5(this.fgv.contrasena.value).toString();
+    return model;
+  }
+
+
+  get fgv() {
+    return this.fgValidator.controls;
   }
 
 }
